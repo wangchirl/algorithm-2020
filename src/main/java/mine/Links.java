4fbd9b71130ae0577ba6a4645099f2fc06210f4e
“用wangqin <757748953@qq.com>
2020-12-13 22:41:01 +0800

    update

--- src/main/java/mine/Links.java
+++ src/main/java/mine/Links.java
@@ -4,6 +4,7 @@ import class02_03.Code01_ReverseList;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * @author shadow
@@ -29,9 +30,132 @@ public class Links {
 	 * 【要求】
 	 * 时间复杂度O(N)，额外空间复杂度O(1)
 	 */
+	// Map 哈希法
+	public static SNode copyNode(SNode head) {
 
-	// 6、将单链表按值划分为左边下、中间相等、右边大的形式
-	// partition - 边界问题，左边无，中间无，右边无
+
+		return null;
+	}
+
+
+
+
+	static class SNode {
+		int value;
+		Node next;
+		Node rand;
+		public SNode(int v) {this.value = v;}
+	}
+
+	// 6、将单链表按值划分为左边小、中间相等、右边大的形式
+	// partition
+	// 6.1 转为数组 - 荷兰国旗划分问题
+	// 6.2 三个区，三个头尾结点相连 - 考虑边界问题 左边无，中间无，右边无
+	public static Node listPartition1(Node head,int pivot) {
+		if(head == null) {
+			return head;
+		}
+		Node cur = head;
+		int i = 0;
+		while (cur != null) {
+			i++;
+			cur = cur.next;
+		}
+		// 转数组
+		Node[] help = new Node[i];
+		cur = head;
+		for (int j = 0; j < help.length; j++) {
+			help[j] = cur;
+			cur = cur.next;
+		}
+		// 荷兰国旗划分
+		partition(help, pivot);
+		// 再将链表连接起来
+		for (int j = 1; j < help.length; j++) {
+			help[j-1].next = help[j];
+		}
+		// 尾部指向null
+		help[help.length - 1].next = null;
+		// 返回链表头部
+		return help[0];
+	}
+
+	private static void partition(Node[] arr, int pivot) {
+		int L = -1;
+		int R = arr.length;
+		int index = 0;
+		while (index < R) {
+			if(arr[index].value > pivot) { // 右扩
+				swap(arr,index,--R);
+			}else if(arr[index].value < pivot) { // 左扩
+				swap(arr,index++,++L);
+			}else {
+				index++;
+			}
+		}
+	}
+
+	private static void swap(Node[] arr,int i,int j) {
+		Node temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+
+
+	// 三区域划分
+	public static Node listPartition2(Node head,int pivot) {
+		// 定义三个区域的指针
+		Node sH = null;
+		Node sT = null;
+		Node eH = null;
+		Node eT = null;
+		Node bH = null;
+		Node bT = null;
+		Node next = null;
+		while (head != null) {
+			// 先记录head的next
+			next = head.next;
+			// 将结点的next值置空
+			head.next = null;
+			// 比较判断
+			if(head.value < pivot) { // 小于区域
+				if(sH == null){
+					sH = head;
+					sT = head;
+				}else {
+					sT.next = head; // 尾部追加 , 尾部后移
+					sT = head;
+				}
+			}else if(head.value == pivot) { // 等于区域
+				if(eH == null) {
+					eH = head;
+					eT = head;
+				}else{
+					eT.next = head;
+					eT = head;
+				}
+			}else { // 大于区域
+				if(bH == null) {
+					bH = head;
+					bT = head;
+				}else {
+					bT.next = head;
+					bT = head;
+				}
+			}
+			head = next; // 下一个元素
+		}
+		// 小于区域的尾连接等于区域的头，等于区域的尾连接大于区域的头
+		if(sT != null) {
+			sT.next = eH; // 小尾接中头
+			eT = eT == null ? sT : eT; // 中尾为空则用小尾顶上
+		}
+		if(eT != null) {
+			eT.next = bH;
+		}
+		return sH != null ? sH : (eH != null ? eH : bH);
+	}
+
 
 	// 5、快慢指针问题
 	// 5.1 输入链表头结点，奇数长度返回中点，偶数长度返回上中点
@@ -39,13 +163,135 @@ public class Links {
 	// 5.3 输入链表头结点，奇数长度返回中点前一个，偶数长度返回上中点前一个
 	// 5.4 输入链表头结点，奇数长度返回中点前一个，偶数长度返回下中点前一个
 
+	// (node.size() - 1) / 2
+	public static Node midOrUpMidNode(Node head) {
+		// 2个结点内都是返回头结点
+		if(head == null || head.next == null || head.next.next == null) {
+			return head;
+		}
+		// 三个结点即以上
+		Node slow = head.next; // 指向当前结点的上一个结点
+		Node fast = head.next.next; // 指向当前结点
+		while (fast.next != null && fast.next.next != null) {
+			slow = slow.next; // 慢指针一次走一个，快指针一次走2个，要保证不为null
+			fast = fast.next.next;
+		}
+		return slow;
+	}
+
+	// node.size() / 2
+	public static Node midOrDownMidNode(Node head) {
+		// 一个以内就是自己
+		if(head == null || head.next == null) {
+			return head;
+		}
+		// 两个即以上
+		Node slow = head.next; // 指向当前结点
+		Node fast = head.next; // 指向当前结点
+		while (fast.next != null && fast.next.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+		}
+		return slow;
+	}
+
+	// (node.size() - 3) / 2
+	public static Node midOrUpMidPreNode(Node head) {
+		// 2个结点内都是自己
+		if(head == null || head.next == null || head.next.next == null) {
+			return head;
+		}
+		Node slow = head; // 指向上一个结点
+		Node fast = head.next.next; // 指向当前结点下一个节点
+		while (fast.next != null && fast.next.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+		}
+		return slow;
+	}
+
+	// (node.size() - 2) / 2
+	public static Node midOrDownMidPreNode(Node head) {
+		// 一个结点内 指向自己
+		if(head == null || head.next == null) {
+			return head;
+		}
+		Node slow = head; // 指向前一个结点
+		Node fast = head.next; // 指向当前结点
+		while (fast.next != null && fast.next.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+		}
+		return slow;
+	}
+
+
+
+
+
 
 	// 4、给定链表头结点，判断链表是否回文结构
-	// 4.1 哈希表方式
+	// 4.1 栈方式
 	// 4.2 修改原链表
+	public static boolean isPalindromeList1(Node head) {
+		Stack<Node> stack = new Stack<>();
+		Node cur = head;
+		while (cur != null) {
+			stack.push(cur);
+			cur = cur.next;
+		}
+		while (head != null) {
+			if(head.value != stack.pop().value){
+				return false;
+			}
+			head = head.next;
+		}
+		return true;
+	}
 
+	// Node1 -> Node2 -> Node3 -> Node4
+	// 折半入栈  Node3 -> Node4
+	public static boolean isPalindromeList2(Node head) {
+		if(head == null || head.next == null){
+			return true;
+		}
+		// 快慢指针，快速找到中点位置
+		Node right = head.next;
+		Node cur = head;
+		while (cur.next != null && cur.next.next != null){
+			right = right.next;
+			cur = cur.next.next;
+		}
+		Stack<Node> stack = new Stack<>();
+		while (right != null){
+			stack.push(right);
+			right = right.next;
+		}
+		while (!stack.isEmpty()) {
+			if(stack.pop().value != head.value) {
+				return false;
+			}
+			head = head.next;
+		}
+		return true;
+	}
+
+	// Node1 -> Node2 -> Node3 -> Node4
+	// Node1 -> Node2 -> null
+	// 			Node2 <- Node3 <- Node4
+	// TODO 改变原链表 - 记得最后要改回来
+	public static boolean isPalindromeList3(Node head) {
+		return false;
+	}
 
 	// 3、能不能不给单链表的头节点，只给想要删除的节点，就能做到在链表上把这个点删掉？
+	public static void deleteSelf(Node node) {
+		if(node == null || node.next == null) {
+			return;
+		}
+		node.value = node.next.value;
+		node.next = node.next.next;
+	}
 
 
 	// 2、删除给定的值
@@ -244,16 +490,44 @@ public class Links {
 			if (!checkDoubleListReverse(list3, node3)) {
 				System.out.println("Oops3!");
 			}
-//
-//			DoubleNode node4 = generateRandomDoubleList(len, value);
-//			List<Integer> list4 = getDoubleListOriginOrder(node4);
-//			node4 = reverseDoubleList(node4);
-//			if (!checkDoubleListReverse(list4, node4)) {
-//				System.out.println("Oops4!");
-//			}
+
+			DoubleNode node4 = generateRandomDoubleList(len, value);
+			List<Integer> list4 = getDoubleListOriginOrder(node4);
+			node4 = reverseDoubleList(node4);
+			if (!checkDoubleListReverse(list4, node4)) {
+				System.out.println("Oops4!");
+			}
 
 		}
 		System.out.println("test finish!");
+
+
+
+		Node head = new Node(9);
+		head.next = new Node(11);
+		head.next.next = new Node(2);
+		head.next.next.next = new Node(4);
+		Node node = head.next.next.next.next = new Node(5);
+		head.next.next.next.next.next = new Node(1);
+
+		deleteGiveValue(head,4);
+		deleteSelf(node);
+		Node cur = head;
+		while (cur != null) {
+			System.out.print(cur.value + " ");
+			cur = cur.next;
+		}
+
+		System.out.println();
+
+		cur = head;
+//		cur = listPartition1(cur,9);
+		cur = listPartition2(cur,9);
+		while (cur != null) {
+			System.out.print(cur.value + " ");
+			cur = cur.next;
+		}
+
 
 	}
 
--- src/main/java/mine/Trees.java
+++ src/main/java/mine/Trees.java
@@ -1,5 +1,11 @@
 package mine;
 
+import class07.Code04_SerializeAndReconstructTree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 /**
  * @author shadow
  * @create 2020-12-06
@@ -44,42 +50,352 @@ public class Trees {
 
 	// 6、求二叉树最宽的层有多少个结点
 
-	// 5、二叉树的序列化与反序列化
+
+
+	// 7、TODO 如何设计一个二叉树的打印
+
+
+	// 6、TODO 多叉树如何序列化问题
+	// 转二叉树 - X 的孩子放在 X的左树右边界
 	/**
-	 * 1）可以用先序或者中序或者后序或者按层遍历，来实现二叉树的序列化
+	 *    a
+	 *  b c d
 	 *
-	 * 2）用了什么方式序列化，就用什么样的方式反序列化
+	 *    a
+	 *  b
+	 *    c
+	 *      d
 	 */
 
+
+
+
+	/**
+	 * 5、二叉树的序列化与反序列化
+	 * 1.先序
+	 * 2.后序
+	 * 3.按层
+	 * 分隔符、占位符
+	 * <p>
+	 * 1）可以用先序或者中序或者后序或者按层遍历，来实现二叉树的序列化
+	 * <p>
+	 * 2）用了什么方式序列化，就用什么样的方式反序列化
+	 * <p>
+	 * 3) 只能通过先序和后序的结果才能进行反序列化
+	 * <p>
+	 * 二叉树可以通过先序、后序或者按层遍历的方式序列化和反序列化，
+	 * 以下代码全部实现了。
+	 * 但是，二叉树无法通过中序遍历的方式实现序列化和反序列化
+	 * 因为不同的两棵树，可能得到同样的中序序列，即便补了空位置也可能一样。
+	 * 比如如下两棵树
+	 * __2
+	 * /
+	 * 1
+	 * 和
+	 * 1__
+	 * \
+	 * 2
+	 * 补足空位置的中序遍历结果都是{ null, 1, null, 2, null}
+	 */
+	// 先序序列化
+	public static Queue<String> preSerial(Node head) {
+		Queue<String> queue = new LinkedList<>();
+		preSerial(head, queue);
+		return queue;
+	}
+
+	private static void preSerial(Node head, Queue<String> queue) {
+		if (head == null) {
+			queue.add(null);
+		} else {
+			queue.add(String.valueOf(head.value));
+			preSerial(head.left, queue);
+			preSerial(head.right, queue);
+		}
+	}
+
+	// 先序反序列化 - 先序建树
+	public static Node preBuildTree(Queue<String> nodes) {
+		if (nodes.isEmpty()) {
+			return null;
+		}
+		return preBuild(nodes);
+	}
+
+	private static Node preBuild(Queue<String> nodes) {
+		String value = nodes.poll();
+		if (value == null) {
+			return null;
+		}
+		Node head = new Node(Integer.parseInt(value));
+		head.left = preBuild(nodes);
+		head.right = preBuild(nodes);
+		return head;
+	}
+
+	// 后序序列化
+	public static Queue<String> postSerial(Node head) {
+		if (head == null) {
+			return null;
+		}
+		Queue<String> queue = new LinkedList<>();
+		postSerial(head, queue);
+		return queue;
+	}
+
+	private static void postSerial(Node head, Queue<String> queue) {
+		if (head == null) {
+			queue.add(null);
+		} else {
+			postSerial(head.left, queue);
+			postSerial(head.right, queue);
+			queue.add(String.valueOf(head.value));
+		}
+	}
+
+	// 后序建树 - 左右中 -stack> 中右左 => 与先序建树 左右先后顺序颠倒一下
+	public static Node postBuildTree(Queue<String> nodes) {
+		if (nodes.isEmpty()) {
+			return null;
+		}
+		// 左右中  ->  stack(中右左)
+		Stack<String> stack = new Stack<>();
+		while (!nodes.isEmpty()) {
+			stack.push(nodes.poll());
+		}
+		return postBuild(stack);
+	}
+
+	private static Node postBuild(Stack<String> nodes) {
+		String value = nodes.pop();
+		if (value == null) {
+			return null;
+		}
+		Node head = new Node(Integer.parseInt(value));
+		head.right = postBuild(nodes);
+		head.left = postBuild(nodes);
+		return head;
+	}
+
+	// 按层序列化
+	public static Queue<String> levelSerial(Node head) {
+		Queue<String> queue = new LinkedList<>();
+		if (head == null) {
+			queue.add(null);
+		} else {
+			queue.add(String.valueOf(head.value)); // 入队
+			Queue<Node> help = new LinkedList<>();
+			help.add(head);
+			while (!help.isEmpty()) {
+				head = help.poll();
+				if (head.left != null) {
+					queue.add(String.valueOf(head.left.value));
+					help.add(head.left);
+				} else {
+					queue.add(null);
+				}
+				if (head.right != null) {
+					queue.add(String.valueOf(head.right.value));
+					help.add(head.right);
+				}else {
+					queue.add(null);
+				}
+			}
+		}
+		return queue;
+	}
+
+	// 按层建树
+	public static Node buildByLevelQueue(Queue<String> levelList) {
+		if (levelList == null || levelList.size() == 0) {
+			return null;
+		}
+		Node head = generateNode(levelList.poll());
+		Queue<Node> queue = new LinkedList<>();
+		if (head != null) {
+			queue.add(head);
+		}
+		Node node = null;
+		while (!queue.isEmpty()) {
+			node = queue.poll();
+			node.left = generateNode(levelList.poll());
+			node.right = generateNode(levelList.poll());
+			if (node.left != null) {
+				queue.add(node.left);
+			}
+			if (node.right != null) {
+				queue.add(node.right);
+			}
+		}
+		return head;
+	}
+
+	public static Node generateNode(String val) {
+		if (val == null) {
+			return null;
+		}
+		return new Node(Integer.valueOf(val));
+	}
 
 	// 4、二叉树的层次遍历 - 队列
+
 	/**
 	 * 1）其实就是宽度优先遍历，用队列
-	 *
+	 * <p>
 	 * 2）可以通过设置flag变量的方式，来发现某一层的结束（看题目）
 	 */
+	// 从左到右依次入队
+	public static void level(Node head) {
+		if (head != null) {
+			Queue<Node> queue = new LinkedList<>();
+			queue.add(head);
+			while (!queue.isEmpty()) {
+				head = queue.poll();
+				System.out.print(head.value + " ");
+				if (head.left != null) {
+					queue.add(head.left);
+				}
+				if (head.right != null) {
+					queue.add(head.right);
+				}
+			}
+		}
+	}
+
+
+	// 理解递归序
+	public static void recursive(Node head) {
+		if (head == null) {
+			return;
+		}
+		// 1 先序
+		recursive(head.left);
+		// 2 中序
+		recursive(head.right);
+		// 3 后序
+	}
+
 
 	/**
 	 * 先序：任何子树的处理顺序都是，先头节点、再左子树、然后右子树
-	 *
+	 * <p>
 	 * 中序：任何子树的处理顺序都是，先左子树、再头节点、然后右子树
-	 *
+	 * <p>
 	 * 后序：任何子树的处理顺序都是，先左子树、再右子树、然后头节点
-	 *
+	 * <p>
 	 * 1）理解递归序
-	 *
+	 * <p>
 	 * 2）先序、中序、后序都可以在递归序的基础上加工出来
-	 *
+	 * <p>
 	 * 3）第一次到达一个节点就打印就是先序、第二次打印即中序、第三次即后序
-	 *
+	 * <p>
 	 * 1）任何递归函数都可以改成非递归
-	 *
+	 * <p>
 	 * 2）自己设计压栈的来实现
 	 */
 	// 3、二叉树的后序遍历
+	// 左->右->头
+	public static void post(Node head) {
+		if (head == null) {
+			return;
+		}
+		post(head.left);
+		post(head.right);
+		System.out.print(head.value + " ");
+	}
+
+	// 压入顺序先压入左,后压入右，弹出时用另外的栈接收
+	public static void postByStack(Node head) {
+		if (head != null) {
+			Stack<Node> stack = new Stack<>();
+			Stack<Node> result = new Stack<>();
+			stack.push(head);
+			while (!stack.isEmpty()) {
+				head = stack.pop();
+				result.push(head); // 另外的栈接收
+				if (head.left != null) { // 有左先压入左
+					stack.push(head.left);
+				}
+				if (head.right != null) {
+					stack.push(head.right);
+				}
+			}
+			// 再弹出
+			while (!result.isEmpty()) {
+				System.out.print(result.pop().value + " ");
+			}
+		}
+	}
 
 	// 2、二叉树的中序遍历
+	// 左->头->右
+	public static void in(Node head) {
+		if (head == null) {
+			return;
+		}
+		in(head.left);
+		System.out.print(head.value + " ");
+		in(head.right);
+	}
+
+	// 这里先压入全部的左，弹出再压入右，后再压入右的全部的左
+	public static void inByStack(Node head) {
+		if (head != null) {
+			Stack<Node> stack = new Stack<>();
+			stack.push(head);
+			while (!stack.isEmpty() || head != null) {
+				// 有左则一直压入
+				if (head != null) {
+					stack.push(head);
+					head = head.left;
+				} else {
+					// 否则弹出打印压入右
+					head = stack.pop();
+					System.out.print(head.value + " ");
+					head = head.right;
+				}
+			}
+		}
+	}
 
 	// 1、二叉树的先序遍历
+	// ->头 ->左 ->右
+	// 递归实现
+	public static void pre(Node head) {
+		if (head == null) {
+			return;
+		}
+		System.out.print(head.value + " ");
+		pre(head.left);
+		pre(head.right);
+	}
+
+	// 非递归实现 - 借助栈
+	public static void preByStack(Node head) {
+		if (head != null) {
+			Stack<Node> stack = new Stack<>();
+			stack.push(head);
+			while (!stack.isEmpty()) {
+				head = stack.pop();
+				System.out.print(head.value + " ");
+				if (head.right != null) { // 有右先压入右
+					stack.push(head.right);
+				}
+				if (head.left != null) {
+					stack.push(head.left);
+				}
+			}
+		}
+	}
+
+	static class Node {
+		int value;
+		Node left;
+		Node right;
+
+		public Node(int v) {
+			this.value = v;
+		}
+	}
 
 }
